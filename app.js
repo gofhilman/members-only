244accf7c4e3d8c5f8c63e0c263cd749cdc0c38c
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-22 13:37:07 +0700

    WIP: add passport.js middleware

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const path = require("node:path");
 const session = require("express-session");
 const PgStore = require("connect-pg-simple")(session);
-
+const pool = require("./db/pool");
+const passport = require("passport");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +12,24 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-const sessionStore = new PgStore({});
+const sessionStore = new PgStore({
+  pool: pool,
+  createTableIfMissing: true,
+});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 14 * 24 * 3600 * 1000,
+    },
+  })
+);
+
+require("./config/passport");
+app.use(passport.session());
 
 app.use((err, _, res, __) => {
   console.error(err);
