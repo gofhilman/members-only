d4b1a4e1b138067e001253dfbae45ee00d2d45f2
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-25 22:02:24 +0700

    fix: set time zone cookie only after login

--- public/modules/main.js
+++ public/modules/main.js
@@ -1,12 +1,12 @@
 const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-if (!document.cookie.includes('timeZoneSet=true')) {
-  fetch('/set-timezone', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ timeZone })
+if (isLoggedIn && !document.cookie.includes("timeZoneSet=true")) {
+  fetch("/set-timezone", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ timeZone }),
   }).then(() => {
-    document.cookie = 'timeZoneSet=true; max-age=86400'; // 1 day
+    document.cookie = "timeZoneSet=true; max-age=86400"; // 1 day
     window.location.reload();
   });
 }
