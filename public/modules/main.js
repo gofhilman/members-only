82b3fea9918b2bed97c8ebf42958ed3be94cfccb
Hilman Fikry <hilman12fikry@gmail.com>
2025-09-25 21:32:34 +0700

    fix: auto refresh if time zone is not set

--- public/modules/main.js
+++ public/modules/main.js
@@ -1,7 +1,12 @@
 const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-fetch('/set-timezone', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ timeZone })
-});
+if (!document.cookie.includes('timeZoneSet=true')) {
+  fetch('/set-timezone', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ timeZone })
+  }).then(() => {
+    document.cookie = 'timeZoneSet=true; max-age=86400'; // 1 day
+    window.location.reload();
+  });
+}
